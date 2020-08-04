53bf01d43ad73bb81e402d671e7dbde5526d45bb
crispthoalex <alex877@gmail.com>
2020-08-04 06:03:45 +0000

    change file by file_line

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -1,12 +1,12 @@
 # Set up client SSH configuration file, so can connect to a server without a password
-file { 'Turn off passwd auth':
-ensure => 'presents',
+file_line { 'Turn off passwd auth':
+ensure => 'present',
 path   => '/etc/ssh/ssh_config',
 line   => 'PasswordAuthentication no',
 }
 
-file { 'Declare identity file':
-ensure => 'presents',
+file_line { 'Declare identity file':
+ensure => 'present',
 path   => '/etc/ssh/ssh_config',
 line   => 'IdentityFile ~/.ssh/holberton',
 }
