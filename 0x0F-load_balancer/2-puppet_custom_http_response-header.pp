2cc1d38f098257ae29aae4ac6c8003f90bf98ac5
crispthoalex <alex877@gmail.com>
2020-08-15 00:51:27 +0000

    Add manifesto to create a custom HTTP header response Create a custom HTTP header response with Puppet.
    * The name of the custom HTTP header is X-Served-By
    * The value of the custom HTTP header the hostname of the server Nginx is running on
    * Configures a brand new Ubuntu machine

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
@@ -0,0 +1,20 @@
+# Install and configure an Nginx server and Add a custom HTTP header
+
+# install nginx
+package { 'nginx':
+ensure  => installed,
+}
+
+# Add custom HTTP header
+file_line { 'Custom HTTP header':
+ensure => 'present',
+after  => 'listen 80 default_server',
+path   => '/etc/nginx/sites-available/default',
+line   => 'add_header X-Served-By $HOSTNAME;',
+}
+
+# Restar nginx to apply change
+service { 'nginx':
+ensure  => running,
+require => package['nginx'],
+}
