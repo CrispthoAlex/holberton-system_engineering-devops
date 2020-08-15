a24951770d8e91c548d3861ba674a8fdccde0ec0
crispthoalex <alex877@gmail.com>
2020-08-15 01:35:53 +0000

    Fix file_line attribute and add requires to blocks

--- 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
@@ -6,15 +6,16 @@ ensure  => installed,
 }
 
 # Add custom HTTP header
-file_line { 'Custom HTTP header':
-ensure => 'present',
-after  => 'listen 80 default_server',
-path   => '/etc/nginx/sites-available/default',
-line   => 'add_header X-Served-By \$HOSTNAME;',
+file_line { 'Custom_HTTP_header':
+ensure  => 'present',
+after   => 'listen 80 default_server',
+path    => '/etc/nginx/sites-available/default',
+line    => "add_header X-Served-By ${hostname};",
+require => Package['nginx'],
 }
 
-# Restar nginx to apply change
+# Restart nginx to apply change
 service { 'nginx':
 ensure  => running,
-require => package['nginx'],
+require => File_line['Custom_HTTP_header'],
 }
