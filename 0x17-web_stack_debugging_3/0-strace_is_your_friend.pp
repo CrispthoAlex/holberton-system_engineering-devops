34dd518552fa39f50e9dca255f50b381a52c94e1
crispthoalex <alex877@gmail.com>
2020-09-30 01:32:01 +0000

    Fix code with exec resource

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,11 +1,6 @@
 # Puppet script to fix internal server Error
 
-file { '/var/www/html/wp-settings.php':
-ensure => present,
-}->
-file_line { 'Replace line in wp-settings':
-path    => '/var/www/html/wp-settings.php',
-replace => true,
-line    => "require_once( ABSPATH . WPINC . \'/class-wp-locale.php\' );",
-match   => 'require_once( ABSPATH . WPINC . \'/class-wp-locale.phpp\' );',
+exec { 'fix_internal_server_error':
+command  => 'sed -i "s/class-wp-locale.phpp/class-wp-locale.php/g" /var/www/html/wp-settings.php',
+provider => shell,
 }
